3407cd48b1fe4e2d7080cc31ca38e12085fe0102
Musie <musse2003@gmail.com>
2018-10-28 23:32:08 +0100

    this one checks also the minimum offer from operators

--- trial/src/trial/Phone_storage.java
+++ trial/src/trial/Phone_storage.java
@@ -7,11 +7,11 @@ public class Phone_storage {
     final static HashMap<String, HashMap<Integer, Double>> hm = new HashMap<String, HashMap<Integer, Double>>();
 	static HashMap<Integer, Double> val;
 	static boolean oper = true;
+	static HashMap<String, Double> values = new HashMap<String, Double>();
 	public static void main(String[] args) {
 		
 		try{
-			
-		while(oper)	{
+		   while(oper){
 			System.out.println("please insert the name of operator");
 			Scanner scan = new Scanner(System.in);
 			String operator = scan.nextLine(); 
@@ -30,33 +30,46 @@ public class Phone_storage {
 		    	 continue;
 		     }
 		   }	
-		System.out.println("passed");
+		//System.out.println("passed");
 
 		System.out.println("Please type the phone number you want to search");
 		Scanner sca = new Scanner(System.in);
-		int phoneno  = sca.nextInt(); 
+		String phoneno  = sca.nextLine(); 
 		String operator1 = "";
-		int prefix = 0;
+		String prefix = "";
+		int prefix_length = 0;
+		Integer longest_prefix = 0;
 		for (Entry<String, HashMap<Integer, Double>> entry : hm.entrySet()) {
-		    System.out.println("VALUE:::" +entry.getValue());
-		        operator1 = entry.getKey();
-		        System.out.println("Value in case 1::" + entry.getKey());
-		        System.out.println("Value in case 2::" + hm.get(operator1).get(phoneno));
-		        
-		        for (Entry<Integer, Double> prefixes : entry.getValue().entrySet()) { 
-	                prefix = prefixes.getKey();
-	                System.out.println("Value in case ::"+ prefixes.getKey());
-	           
+			operator1 = entry.getKey();
+	        System.out.println("name of operator::" + entry.getKey());
+		    System.out.println("price lists in this operator:::" +entry.getValue());
+	        for (Entry<Integer, Double> prefixes : entry.getValue().entrySet()) { 
+                prefix = Integer.toString(prefixes.getKey());
+                if((phoneno.startsWith(prefix)) && (prefix.length() > prefix_length)){
+                	prefix_length = prefix.length();
+                	longest_prefix = Integer.parseInt(prefix);
+                }
+                //System.out.println("phone prefix ::"+ prefixes.getKey());
 		        }
+		        Double price = hm.get(operator1).get(longest_prefix);
+		        values.put(operator1, price);
 		    }
 		
+		Entry<String, Double> min = null;
+		for (Entry<String, Double> entry : values.entrySet()) {
+		    if (min == null || min.getValue() > entry.getValue()) {
+		        min = entry;
+		    }
+		}
+		System.out.println("Operator " + min.getKey() + "has the minimum price of " + min.getValue() + "cents");
+		
 	   } catch (Exception e) {
-				System.out.println(e);
-		  }
+		      System.out.println(e);
+	     }
 	}
     public static void func(String operator){
     	val = new HashMap<Integer, Double>();
-    	System.out.println("insert phone prefix and price per minute, please type 'finish' to stop inserting");
+    	System.out.println("insert phone prefix and price per minute, please type 'finish' or any word to stop inserting");
 		Scanner scann = new Scanner(System.in);
 		while(scann.hasNextInt()){
 			int key = scann.nextInt(); 
